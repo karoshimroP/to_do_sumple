0d860427feb0d4153ba259822020ead6bab409a0
karoshimroP <i.nono.soccer@gmail.com>
2020-06-13 10:44:00 +0900

    first commit

---
+++ to_do_sumple/AddController.swift
@@ -0,0 +1,39 @@
+//
+//  AddController.swift
+//  to_do_sumple
+//
+//  Created by itsuki kaji on 2020/06/11.
+//  Copyright © 2020 itsuki kaji. All rights reserved.
+//
+
+import UIKit
+
+//変数の設置
+var TodoKobetsunonakami = [String]()
+
+class AddController: UIViewController {
+
+    //テキストフィールドの設定
+    @IBOutlet weak var TodoTextField: UITextField!
+
+    //追加ボタンの設定
+    @IBAction func TodoAddButten(_ sender: Any) {
+        //変数に入力内容を入れる
+        TodoKobetsunonakami.append(TodoTextField.text!)
+        //追加ボタンを押したらフィールドを空にする
+        TodoTextField.text = ""
+        //変数の中身をUDに追加
+        UserDefaults.standard.set( TodoKobetsunonakami, forKey: "TodoList" )
+    }
+
+
+    //最初からあるコード
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //最初からあるコード
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
