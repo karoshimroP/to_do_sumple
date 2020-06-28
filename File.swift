6353a1f6a1e040f1df0c176136daa074580b2778
かじmac <i.nono.soccer@gmail.com>
2020-06-29 02:02:16 +0900

    chart test

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  to_do_sumple
+//
+//  Created by itsuki on 2020/06/28.
+//  Copyright © 2020 itsuki kaji. All rights reserved.
+//
+
+import Foundation
---
+++ to_do_sumple/ChartController.swift
@@ -0,0 +1,14 @@
+//
+//  ChartController.swift
+//  to_do_sumple
+//
+//  Created by itsuki on 2020/06/28.
+//  Copyright © 2020 itsuki kaji. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class ChartController: UIViewController {
+
+}
