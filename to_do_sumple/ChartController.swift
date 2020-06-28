ee73a1be26176e72d9e928e79af2a6dea5006682
かじmac <i.nono.soccer@gmail.com>
2020-06-29 02:45:13 +0900

    円グラフのテスト表示

--- to_do_sumple/ChartController.swift
+++ to_do_sumple/ChartController.swift
@@ -9,6 +9,41 @@
 import UIKit
 import Charts
 
-class ChartController: UIViewController {
+class ChartController: UIViewController{
 
+    @IBOutlet weak var PieChartView: PieChartView!
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           // 円グラフの中心に表示するタイトル
+           self.PieChartView.centerText = "テストデータ"
+           
+           // グラフに表示するデータのタイトルと値
+           let dataEntries = [
+               PieChartDataEntry(value: 40, label: "A"),
+               PieChartDataEntry(value: 35, label: "B"),
+               PieChartDataEntry(value: 25, label: "C")
+           ]
+           
+        let dataSet = PieChartDataSet(entries: dataEntries, label: "テストデータ")
+
+           // グラフの色
+           dataSet.colors = ChartColorTemplates.vordiplom()
+           // グラフのデータの値の色
+           dataSet.valueTextColor = UIColor.black
+           // グラフのデータのタイトルの色
+           dataSet.entryLabelColor = UIColor.black
+
+           self.PieChartView.data = PieChartData(dataSet: dataSet)
+           
+           // データを％表示にする
+           let formatter = NumberFormatter()
+           formatter.numberStyle = .percent
+           formatter.maximumFractionDigits = 2
+           formatter.multiplier = 1.0
+           self.PieChartView.data?.setValueFormatter(DefaultValueFormatter(formatter: formatter))
+           self.PieChartView.usePercentValuesEnabled = true
+           
+//           view.addSubview(self.PieChartView)
+    }
 }
